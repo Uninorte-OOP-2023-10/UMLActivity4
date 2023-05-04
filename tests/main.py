a9087fafd33f799d909c11c09786c8ddb6a288ec
Eduardo Angulo <edangulo@uninorte.edu.co>
2023-05-04 07:25:55 -0500

    Removed unused import

--- tests/main.py
+++ tests/main.py
@@ -6,7 +6,5 @@ sys.path.insert(2, os.path.join(os.path.abspath(os.pardir), 'src'))
 
 import unittest
 
-from tests.core.hospital import HospitalTest
-
 if __name__ == '__main__':
     unittest.main()
